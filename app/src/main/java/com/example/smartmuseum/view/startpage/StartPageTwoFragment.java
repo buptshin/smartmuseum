1c2db7a0ccc5aaa3746b60ff84a20e4a7e1651dc
myxdd <myxdd1997@163.com>
2020-05-14 16:03:20 +0800

    “导览”“注册”“登录”“忘记密码”页面+id命名

---
+++ app/src/main/java/com/example/smartmuseum/view/startpage/StartPageTwoFragment.java
@@ -0,0 +1,32 @@
+package com.example.smartmuseum.view.startpage;
+
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.smartmuseum.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class StartPageTwoFragment extends Fragment {
+
+
+    public StartPageTwoFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_start_page_two, container, false);
+    }
+
+}
